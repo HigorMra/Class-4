fe571aaa43253b7efa8317562299378bfb0838ac
Higor Moreira <167116708+HigorMra@users.noreply.github.com>
2024-04-16 16:15:48 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+minha_string = "teXto querido,\n texto querido 2,\ntexto querido 3."
+
+               #0123456789111
+               #          012
+
+print(minha_string)
+
+print(f'escrever meu {minha_string} na minha bunda')
+print(minha_string.upper())
+print(minha_string.lower())
+print(minha_string.capitalize())
+print(minha_string.isupper())
+print(minha_string.islower())
+print(minha_string.strip())
+print(minha_string.replace('teXto' , 'amigo'))
+print(minha_string.replace('X' , 'x',1))
+print(minha_string.islower())
+print(len(minha_string))
+print(len(minha_string))
+print(minha_string[8:13])
+print(minha_string.index('ido'))
+
+x = 'texto' in minha_string
+print(x)
